62c6d45650ced7dda98ab3362d4a7cc0283c5140
Oludamola <oludamolaijimade@gmail.com>
2026-02-02 00:03:45 +0100

    fix: update layout issues

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,47 +1,42 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Sidebar } from '../components/Sidebar/Sidebar';
-import { sidebarConfig } from '../constants/sidebarConfig';
 import DashboardNav from '../components/Navbar/dashboardNav';
 import './MainLayout.scss';
 import TextInput from '../components/Form/TextInput';
 import searchIcon from '../assets/icons/search.svg';
+import { AppDispatch, RootState } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchTerm } from '../redux/slices/usersSlice';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 export const MainLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
- 
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const closeSidebar = () => setIsSidebarOpen(false);
 
-   
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  const dispatch = useDispatch<AppDispatch>();
+    const searchTerm = useSelector((state: RootState) => state.users.searchTerm);
 
-    const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
-  
   return (
     <div className="main-layout">
-      <Sidebar
-        config={sidebarConfig}
-        isOpen={isSidebarOpen}
-        onClose={closeSidebar}
-      />
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
 
       <DashboardNav onMenuToggle={toggleSidebar} />
 
       <main className="main-content">
         <div className="dashboard-nav__search-wrapper mobile-only">
-            <TextInput
-              name="search"
-              type="search"
-              placeholder="Search for anything"
-              inputClassName="dashboard-nav__search-input"
-            />
-             <div className="dashboard-nav__search-action">
-              <img src={searchIcon} alt="search-icon" />
-            </div>
+          <TextInput
+            name="search"
+            type="search"
+            placeholder="Search for anything"
+            inputClassName="dashboard-nav__search-input"
+            value={searchTerm}
+            onChange={(e) => dispatch(setSearchTerm(e.target.value))}
+          />
+          <div className="dashboard-nav__search-action">
+            <img src={searchIcon} alt="search-icon" />
           </div>
+        </div>
         <Outlet />
       </main>
     </div>
